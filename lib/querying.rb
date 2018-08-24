a957059d6e0c0de0e625749217c39cdba5da3ce3
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-24 17:20:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,8 +3,8 @@ def select_books_titles_and_years_in_first_series_order_by_year
 FROM books
 INNER JOIN series
 ON book_series_id.id = series.id
-WHERE series_id = 
-GROUP BY(projects.title);
+WHERE series_id = 1
+GROUP BY(books.year);
 "
 end
 
