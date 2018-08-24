37758d39ad5c050b8c8ed045869ea70565117386
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-24 17:29:07 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,8 +11,7 @@ end
 def select_name_and_motto_of_char_with_longest_motto
 "SELECT characters.name, characters.motto
 FROM characters
-WHERE LENGTH(characters.motto) DESC
-LIMIT 1;
+WHERE LENGTH(characters.motto) DESC LIMIT 1;
 "
 end
 
