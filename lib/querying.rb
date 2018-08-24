f10225270b82eee64bdc4f67e897da90d28657b3
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-24 17:38:29 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,8 +24,8 @@ ORDER BY MAX(species) LIMIT 1;
 end
 
 def select_name_and_series_subgenres_of_authors
-  "
-  "
+"SELECT authors.name 
+"
 end
 
 def select_series_title_with_most_human_characters
