a69a4800cda97653857e422735cdc3fca041f0a0
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-24 18:29:54 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,8 +38,8 @@ def select_series_title_with_most_human_characters
 FROM series
 INNER JOIN characters
 ON series.id = characters.series_id
-WHERE characters.species = 'human'
-GROUP BY characters.species; 
+HAVING characters.species = 'human'
+GROUP BY characters.species;
 "
 end
 
