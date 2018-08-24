4b2b423992bf9b96e5e266913a5d67b55a236a44
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-24 18:09:38 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,7 +41,7 @@ def select_character_names_and_number_of_books_they_are_in
 "SELECT characters.name, SUM(character_books.book_id)
 FROM characters
 INNER JOIN character_books
-ON characters.id = character_books.character_id
+ON characters.id = character_books.character_id;
 
 "
 
