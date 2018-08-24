68ea6d7bbad544111a35a4e11b3f887ab65dceae
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-24 17:17:40 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,10 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+"SELECT books.title, books.year
+FROM books
+INNER JOIN pledges
+ON projects.id = pledges.project_id
+GROUP BY(projects.title);
+"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
