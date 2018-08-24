d8b0421efd68d02adf34524694095b7aa112467b
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-24 17:34:34 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ end
 def select_value_and_count_of_most_prolific_species
 " SELECT characters.species, COUNT(characters.species)
 FROM characters
-ORDER BY 
+ORDER BY MAX(species) LIMIT 1;
 "
 end
 
