60ed656bbd5fcf729f83388f9f5a480ab548222e
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-24 18:29:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,9 +36,10 @@ end
 def select_series_title_with_most_human_characters
 "SELECT series.title
 FROM series
-INNER JOIN characters (SELECT characters.species = 'human') 
-ON series.id = characters.series_id;
-
+INNER JOIN characters
+ON series.id = characters.series_id
+WHERE characters.species = 'human'
+GROUP BY characters.species; 
 "
 end
 
