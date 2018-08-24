70d942b85cb938a439a78b1dc2a1e2b0adc7b609
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-24 17:32:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+" SELECT characters.species, characters.
+"
 end
 
 def select_name_and_series_subgenres_of_authors
