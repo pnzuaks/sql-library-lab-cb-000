7252ca27e8c03a299e7ed41e1b269d7f32b7a52b
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-24 18:02:34 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,7 +38,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-"SELECT characters.name, SUM(character_books.character_id)
+"SELECT characters.name, character_books.character_id
 FROM characters
 INNER JOIN character_books
 ON characters.id = character_books.character_id
