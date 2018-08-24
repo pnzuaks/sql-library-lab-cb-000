5b0288d106f1f6f05c5efb1d831b29a3eafff89b
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-24 16:55:29 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,22 +4,36 @@ CREATE TABLE series ( id INTEGER PRIMARY KEY,
   subgenre_id INTEGER,
 );
 
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+);
 
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+);
 
+ CREATE TABLE books (
+   id INTEGER PRIMARY KEY,
+   title TEXT,
+   year INTEGER,
+   series_id INTEGER,
+ );
 
+ CREATE TABLE characters (
+   id INTEGER PRIMARY KEY,
+   name TEXT,
+   motto TEXT,
+   species TEXT,
+   author_id INTEGER,
+   series_id INTEGER,
+ );
 
- series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
+ CREATE TABLE character_books (
+   id INTEGER PRIMARY KEY,
+   character_id INTEGER,
+   book_id INTEGER,
+ );
 
- subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
-
- authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
-
- books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
-
- characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
-
- characters (id, name, motto, species, author_id, series_id) VALUES (5, "Character One", "motto one", "cylon", 2, 2), (6, "Character Two", "motto two", "human", 2, 2), (7, "Character Three", "motto three", "cylon", 2, 2), (8, "Character Four", "motto four", "cylon", 2, 2);
-
- character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
-
- character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
+ 
